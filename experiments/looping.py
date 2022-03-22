c0e3c389634a456c825e0e538e75492d53c60ae6
lpecyna <leszek.pecyna@gmail.com>
2022-03-22 22:46:34 +0000

    Cleaning files

--- experiments/looping.py
+++ experiments/looping.py
@@ -1,7 +1,7 @@
 import os
 
-for i in range(1,10):
-    print("!!!!!!!!!!!!!!!!!LOOP: i: ", i, " !!!!!!!!!!!!!!!!!!!!")
+for i in range(1, 10):
+    print("-------------------LOOP: i: ", i, " !--------------------------")
     if i == 0:
         command = "python experiments/run_curl.py --log_dir './data/curl"+str(i)+"/' --seed -1 --save_video True"
     else:
